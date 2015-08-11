ba75a4f7e466068faa4e8c62b7991db54ae10863
Droxx <michaelstocks1@gmail.com>
2015-08-11 20:53:04 +0100

    Added auto date picker to add-new-transaction dialog

--- TravelBudget/app/src/main/java/com/droxx/dev/travelbudget/add_new_transaction.java
+++ TravelBudget/app/src/main/java/com/droxx/dev/travelbudget/add_new_transaction.java
@@ -1,13 +1,24 @@
 package com.droxx.dev.travelbudget;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class add_new_transaction extends AppCompatActivity {
+
+    EditText dateText;
+    Calendar myCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,38 @@ public class add_new_transaction extends AppCompatActivity {
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
         textView.setAdapter(adapter);
+
+        myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth){
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        dateText = (EditText)findViewById(R.id.transaction_date);
+
+        dateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(add_new_transaction.this, date, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+    }
+
+    private void updateLabel(){
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.UK);
+        dateText.setText(sdf.format(myCalendar.getTime()));
     }
 
     @Override
